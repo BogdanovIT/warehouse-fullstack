f93a47d53110d02e27a05e0b5c9593ef94dc0eca
Bogdanov Andrey <cheshir2006@yandex.ru>
2026-06-01 17:58:13 +0300

    создаем админ-панель

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -23,7 +23,7 @@ router.get('/:date', async (req, res) => {
         console.log('existing:', existing.length, 'записей')
         console.log('BE attendanceMap', attendanceMap)
         const attendanceMap = {}
-        console.log('AFT attendanceMap', attendanceMap)
+        
         existing.forEach(a => { attendanceMap[a.employeeId] = a })
         const result = employees.map(emp => {
             const att = attendanceMap[emp.id]
@@ -51,6 +51,7 @@ router.get('/:date', async (req, res) => {
                 comment: ''
             }
         })
+        console.log('AFT attendanceMap', attendanceMap)
         res.json(result)
     } catch (error) {
         res.status(500).json({ message: 'Ошибка сервера', error: error.message})
