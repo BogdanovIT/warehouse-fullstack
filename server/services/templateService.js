9ccc3088038eff340e4f44655d3ced1e8d8cd78e
Bogdanov Andrey <cheshir2006@yandex.ru>
2026-06-07 10:49:03 +0300

    создаем админ-панель

--- server/services/templateService.js
+++ server/services/templateService.js
@@ -1,7 +1,7 @@
 import ExcelJS from 'exceljs'
 
 class TemplateService {
-    async generateDefectExcel(parsedData){
+    async generateBreezExcel(parsedData){
         const templatePath = '/home/abogdanov/Mobile_Storekeeper/assets/template.xlsx'
                 const workbook = new ExcelJS.Workbook()
                 await workbook.xlsx.readFile(templatePath)
@@ -15,13 +15,37 @@ class TemplateService {
                 worksheet.getCell('X33').value = parsedData.productName
                 worksheet.getCell('R51').value = parsedData.comment
                 worksheet.getCell('D44').value = parsedData.serialNumber
-                if (!parsedData.isSOH)
-                {worksheet.getCell('A51').value = parsedData.sortValue}
+                worksheet.getCell('A51').value = parsedData.sortValue
                 worksheet.getCell('AH11').value = [parsedData.docPrefix, parsedData.aktNumber].join('')
                 worksheet.getCell('AH11').alignment ={horizontal: 'left'}
                 worksheet.getCell('B65').value = parsedData.cell
         
                 return workbook.xlsx.writeBuffer()
+    }
+        async generateSohExcel (parsedData) {
+            const templatePath = '/home/abogdanov/Mobile_Storekeeper/assets/template_soh.xlsx'
+            const workbook = new ExcelJS.Workbook()
+            await workbook.xlsx.readFile(templatePath)
+            const worksheet = workbook.getWorksheet(1)
+            worksheet.getCell('A4').value = parsedData.currentDate
+            worksheet.getCell('A33').value = parsedData.numberSSCC
+            worksheet.getCell('BD122').value = parsedData.defectNumber
+            worksheet.getCell('BF4').value = parsedData.place
+            worksheet.getCell('AV26').value = parsedData.docNumber
+            worksheet.getCell('A38').value = [parsedData.inputValuePrefix,parsedData.articleCode].join('')
+            worksheet.getCell('X33').value = parsedData.productName
+            worksheet.getCell('A53').value = parsedData.comment
+            worksheet.getCell('D44').value = parsedData.serialNumber
+            worksheet.getCell('AH11').value = [parsedData.docPrefix, parsedData.aktNumber].join('')
+            worksheet.getCell('AH11').alignment ={horizontal: 'left'}
+            worksheet.getCell('B64').value = parsedData.cell
+            return workbook.xlsx.writeBuffer()
+    }
+    async generateDefectExcel(parsedData) {
+        if (parsedData.isSOH) {
+            return this.generateSohExcel(parsedData)
+        }
+        return this.generateBreezExcel(parsedData)
     }
 }
 export default new TemplateService()
